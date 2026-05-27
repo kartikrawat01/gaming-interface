c0b51bad83c2fc6c6b27c05f0355e40b6a7a3ebc
kartikrawat01 <56831241+kartikrawat01@users.noreply.github.com>
2026-05-27 12:19:25 +0530

    Update AuthModal.tsx

--- src/components/ui/AuthModal.tsx
+++ src/components/ui/AuthModal.tsx
@@ -1,5 +1,5 @@
 import { useState, memo } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../../supabaseClient';
 
 function AuthModalComponent({ onClose, setUser }: any) {
 
