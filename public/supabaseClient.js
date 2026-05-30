145326ca3bade64f983cb4ef70be3c63458ead9f
Anjali <mk1732377@gmail.com>
2026-05-30 17:35:07 +0530

    supabase

--- public/supabaseClient.js
+++ public/supabaseClient.js
@@ -4,7 +4,16 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 window.supabaseClient =
   window.supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_ANON_KEY,
+    {
+    auth: {
+      persistSession: true,
+      storageKey: 'sb-session',
+      storage: window.localStorage,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
   );
 
 window.sb =
